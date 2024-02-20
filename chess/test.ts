e4ce18aced969d2945ab2d8eae8b4587cdfb6054
Mate Jenei <mate_jenei@epam.com>
2024-02-20 23:28:21 +0100

    add typescript and ts-node

--- chess/test.ts
+++ chess/test.ts
@@ -1,6 +1,6 @@
 import { it } from "node:test";
 import assert from "node:assert";
-import ChessBoard from "./ChessBoard.js";
+import ChessBoard from "./ChessBoard.ts";
 
 it("empty board should have zero pieces", () => {
   assert.strictEqual(0, new ChessBoard().countPieces());
