4cebd58b7b857587ec5a09f43387acb4ff949b02
Denes Javorek <v-djavorek@expediagroup.com>
2024-06-05 13:13:17 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:chess/Game/Game.ts

--- chess/Game/Game.ts
+++ chess/Game/Game.ts
@@ -1,5 +1,11 @@
+import { MovementError } from "../ChessBoard";
+
 class Game {
   constructor() {}
+
+  move(startLocation: string, targetLocation: string) {
+    throw MovementError;
+  }
 }
 
 export default Game;
